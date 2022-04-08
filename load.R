7ee54344340d9cbb45c01fd436f08c933d67db8d
Sean Boots <sean.boots@gmail.com>
2022-04-08 16:42:40 -0700

    Minor cleanup to column definitions

--- load.R
+++ load.R
@@ -5,8 +5,7 @@ library(skimr)
 library(janitor)
 library(purrr)
 
-# reference_number	procurement_id	vendor_name	vendor_postal_code	buyer_name	contract_date	economic_object_code	description_en	description_fr	contract_period_start	delivery_date	contract_value	original_value	amendment_value	comments_en	comments_fr	additional_comments_en	additional_comments_fr	agreement_type_code	trade_agreement	land_claims	commodity_type	commodity_code	country_of_vendor	solicitation_procedure	limited_tendering_reason	trade_agreement_exceptions	aboriginal_business	aboriginal_business_incidental	intellectual_property	potential_commercial_exploitation	former_public_servant	contracting_entity	standing_offer_number	instrument_type	ministers_office	number_of_bids	article_6_exceptions	award_criteria	socioeconomic_indicator	reporting_period	owner_org	owner_org_title
-
+# Define column types for each column in the contracts dataset
 contract_col_types <- cols(
   
   reference_number = col_character(),
@@ -36,10 +35,10 @@ contract_col_types <- cols(
   agreement_type_code = col_character(),
   trade_agreement = col_character(),
   land_claims = col_character(),
-  commodity_type = col_character(),
+  commodity_type = col_factor(),
   commodity_code = col_character(),
   country_of_vendor = col_character(),
-  solicitation_procedure = col_character(),
+  solicitation_procedure = col_factor(),
   limited_tendering_reason = col_character(),
   trade_agreement_exceptions = col_character(),
   aboriginal_business = col_character(),
@@ -49,7 +48,7 @@ contract_col_types <- cols(
   former_public_servant = col_character(),
   contracting_entity = col_character(),
   standing_offer_number = col_character(),
-  instrument_type = col_character(),
+  instrument_type = col_factor(),
   ministers_office = col_character(),
   number_of_bids = col_number(),
   article_6_exceptions = col_character(),
