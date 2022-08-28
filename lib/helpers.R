9c78878667ca23d1682ee22515adc443817835b2
Sean Boots <sean.boots@gmail.com>
2022-08-27 20:20:09 -0700

    Helper function to load then write out the same CSV file

--- lib/helpers.R
+++ lib/helpers.R
@@ -262,6 +262,20 @@ export_log_entries <- function(location = "data/out/run_log.csv") {
 }
 
 
+# Reload and save CSV file ======================
+
+# To avoid formatting issues introduced by Google Sheets, LibreOffice, or Excel when Git-committing CSV files
+regenerate_csv_file <- function(filename) {
+  data <- read_csv(filename)
+  
+  # Rudimentary error check to avoid writing out again if it didn't load properly:
+  if(count(data) > 0) {
+    data %>%
+      write_csv(filename)
+  }
+}
+
+
 # Find helpers for contract calculation troubleshooting ====
 
 find_selection_helper <- function(df) {
